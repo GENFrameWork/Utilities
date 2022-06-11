89e22aec36e209fbe8f7877dcf3010e3479463a7
Sir Graham <sirgraham@endorasoft.com>
2022-06-11 20:41:38 +0200

    First Version of New APP Creator

---
+++ NewAPPCreator/Unit1.cpp
@@ -0,0 +1,161 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+
+#include "XVector.h"
+#include "XBuffer.h"
+#include "XPathsManager.h"
+#include "XWINDOWSFactory.h"
+#include "XWINDOWSSleep.h"
+#include "XWINDOWSSystem.h"
+#include "XWINDOWSThread.h"
+#include "XWINDOWSDateTime.h"
+#include "XWINDOWSTrace.h"
+#include "XFileCFG.h"
+#include "XString.h"
+#include "XTrace.h"
+#include "XDir.h"
+#include "XFile.h"
+
+#include "Unit1.h"
+
+//---------------------------------------------------------------------------
+
+#pragma package(smart_init)
+#pragma link "cdiroutl"
+#pragma resource "*.dfm"
+
+TForm1 *Form1;
+
+//---------------------------------------------------------------------------
+
+__fastcall TForm1::TForm1(TComponent* Owner) : TForm(Owner)
+{
+
+  XFACTORY::SetInstance(new XWINDOWSFACTORY());
+  if(!XFACTORY::GetIsInstanced()) return;
+
+  XSLEEP::SetInstance(new XWINDOWSSLEEP());
+  if(!XSLEEP::GetIsInstanced()) return;
+
+  XSYSTEM::SetInstance(new XWINDOWSSYSTEM());
+  if(!XSYSTEM::GetIsInstanced()) return;
+
+  XBUFFER::SetHardwareUseLittleEndian(GEN_XSYSTEM.HardwareUseLittleEndian());
+
+  AnsiString exefile = Application->ExeName;
+  AnsiString exepath = ExtractFilePath(exefile);
+  XPATH      xpath;
+
+  xpath  = exepath.c_str();
+  xpath += NEWAPPCREATOR_ROOTDIR;
+
+  GEN_XPATHSMANAGER.AddPathSection(XPATHSMANAGERSECTIONTYPE_ROOT, xpath);
+
+  cfg  = new NEWAPPCREATOR_CFG(NEWAPPCREATOR_CFGNAMEFILE);
+	if(!cfg) return;
+
+
+  Left                            = cfg->GetXPos();
+  Top                             = cfg->GetYPos();
+
+  EditAppPath->Text               = cfg->GetAppPath()->Get();
+  EditGENPath->Text               = cfg->GetGENPath()->Get();
+  EditCopyright->Text             = cfg->GetAppCopyright()->Get();
+
+  RadioGroupAppType->ItemIndex    = cfg->GetAPPType();
+  RadioGroupAppFormat->ItemIndex  = cfg->GetAPPFormat();
+
+  CheckBoxTrace->State            = cfg->GetAddTraceSystem()?cbChecked:cbUnchecked;
+  CheckBoxLog->State              = cfg->GetAddLogSystem()?cbChecked:cbUnchecked;
+  CheckBoxMemControl->State       = cfg->GetAddMemCtrlSystem()?cbChecked:cbUnchecked;
+  CheckBoxCFGFile->State          = cfg->GetAddCFGSystem()?cbChecked:cbUnchecked;
+
+
+
+  CDirectoryOutline->Visible      = false;
+  CDirectoryOutline->Left         = 96;
+  CDirectoryOutline->Top          = 32;
+
+  ButtonCreate->Enabled           = false;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::ButtonExitClick(TObject *Sender)
+{
+  Close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormCreate(TObject *Sender)
+{
+   VersionLabel->Caption = NEWAPPCREATOR_VERSIONLABEL;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::EditAppPathEnter(TObject *Sender)
+{
+  CDirectoryOutline->Visible  = true;
+
+  CDirectoryOutline->Left     = NEWAPPCREATOR_SELECDIRECTORY_XPOS;
+  CDirectoryOutline->Top      = NEWAPPCREATOR_SELECDIRECTORY_APP_YPOS;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::CDirectoryOutlineDblClick(TObject *Sender)
+{
+  CDirectoryOutline->Visible  = false;
+
+  if(CDirectoryOutline->Top == NEWAPPCREATOR_SELECDIRECTORY_APP_YPOS)
+        EditAppPath->Text = CDirectoryOutline->Directory;
+   else EditGENPath->Text = CDirectoryOutline->Directory;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::EditGENPathEnter(TObject *Sender)
+{
+  CDirectoryOutline->Visible  = true;
+
+  CDirectoryOutline->Left     = NEWAPPCREATOR_SELECDIRECTORY_XPOS;
+  CDirectoryOutline->Top      = NEWAPPCREATOR_SELECDIRECTORY_GEN_YPOS;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::TimerCheckStateButtonCreateTimer(TObject *Sender)
+{
+  ButtonCreate->Enabled = (EditAppPath->Text.Length() && EditGENPath->Text.Length() && EditAppName->Text.Length())?true:false;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormClose(TObject *Sender, TCloseAction &Action)
+{
+  cfg->SetXPos(Left);
+  cfg->SetYPos(Top);
+
+  cfg->GetAppPath()->Set(EditAppPath->Text.c_str());
+  cfg->GetGENPath()->Set(EditGENPath->Text.c_str());
+  cfg->GetAppCopyright()->Set(EditCopyright->Text.c_str());
+
+  cfg->SetAPPType(RadioGroupAppType->ItemIndex);
+  cfg->SetAPPFormat(RadioGroupAppFormat->ItemIndex);
+
+  cfg->SetAddTraceSystem((CheckBoxTrace->State==cbChecked)?true:false);
+  cfg->SetAddMemCtrlSystem((CheckBoxMemControl->State==cbChecked)?true:false);
+  cfg->SetAddLogSystem((CheckBoxLog->State==cbChecked)?true:false);
+  cfg->SetAddCFGSystem((CheckBoxCFGFile->State==cbChecked)?true:false);
+
+
+  cfg->Save();
+
+//if(DIOFACTORY::GetIsInstanced()) DIOFACTORY::DelInstance();
+
+  if(XSLEEP::GetIsInstanced()) XSLEEP::DelInstance();
+
+  if(XSYSTEM::GetIsInstanced()) XSYSTEM::DelInstance();
+
+  if(XFACTORY::GetIsInstanced()) XFACTORY::DelInstance();
+}
+//---------------------------------------------------------------------------
+
---
+++ NewAPPCreator/Unit1.h
@@ -0,0 +1,88 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+
+//---------------------------------------------------------------------------
+
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Buttons.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+#include <Grids.hpp>
+#include <Outline.hpp>
+#include <Dialogs.hpp>
+#include <cdiroutl.h>
+
+#include "XBase.h"
+#include "XPath.h"
+#include "XString.h"
+#include "XVector.h"
+
+#include "NewAPPCreator_CFG.h"
+
+//---------------------------------------------------------------------------
+
+#define NEWAPPCREATOR_CFGNAMEFILE	 	 			        __L("NewAPPCreator")
+#define NEWAPPCREATOR_VERSIONLABEL                __L("Version 0.1.0")
+#define NEWAPPCREATOR_ROOTDIR                     __L("assets")
+
+
+#define NEWAPPCREATOR_SELECDIRECTORY_XPOS         96
+#define NEWAPPCREATOR_SELECDIRECTORY_APP_YPOS     32
+#define NEWAPPCREATOR_SELECDIRECTORY_GEN_YPOS     96
+
+//---------------------------------------------------------------------------
+
+class TForm1 : public TForm
+{
+  __published:	// IDE-managed Components
+
+    TBitBtn*                ButtonExit;
+    TBitBtn*                ButtonCreate;
+    TLabel*                 VersionLabel;
+    TImage*                 IMGMain;
+    TEdit*                  EditAppName;
+    TLabel*                 LabelAppName;
+    TLabel*                 LabelGENPath;
+    TEdit*                  EditGENPath;
+    TCDirectoryOutline*     CDirectoryOutline;
+    TRadioGroup*            RadioGroupAppType;
+    TEdit*                  EditAppPath;
+    TLabel*                 LabelAppPath;
+    TTimer*                 TimerCheckStateButtonCreate;
+    TCheckBox*              CheckBoxLog;
+    TCheckBox*              CheckBoxTrace;
+    TEdit*                  EditCopyright;
+    TLabel*                 LabelCopyright;
+    TCheckBox*              CheckBoxCFGFile;
+    TCheckBox*              CheckBoxMemControl;
+    TRadioGroup*            RadioGroupAppFormat;
+
+    void __fastcall         ButtonExitClick                     (TObject *Sender);
+    void __fastcall         FormCreate                          (TObject *Sender);
+    void __fastcall         EditAppPathEnter                    (TObject *Sender);
+    void __fastcall         CDirectoryOutlineDblClick           (TObject *Sender);
+    void __fastcall         EditGENPathEnter                    (TObject *Sender);
+    void __fastcall         TimerCheckStateButtonCreateTimer    (TObject *Sender);
+    void __fastcall         FormClose                           (TObject *Sender, TCloseAction &Action);
+
+  private:	// User declarations
+
+    NEWAPPCREATOR_CFG*		  cfg;
+
+  public:		// User declarations
+
+    __fastcall              TForm1                              (TComponent* Owner);
+};
+
+//---------------------------------------------------------------------------
+
+extern PACKAGE TForm1 *Form1;
+
+//---------------------------------------------------------------------------
+
+#endif
